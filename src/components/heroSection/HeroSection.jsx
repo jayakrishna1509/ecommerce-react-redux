52b44e7190d7d6d4d1e527c80f7cd1c72ceffc9b
jayakrishna1509 <jayakrishnap05789@gmail.com>
2025-10-17 09:58:58 +0530

    Ecommerce-React-Redux

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import banner from '../../assets/images/banner.png'
 
 const HeroSection = () => {
   return (
     <div className="w-full">
       <div className="w-full overflow-hidden">
-        <img 
-          className='w-full h-auto object-contain' 
-          src="./src/assets/images/banner.png" 
-          alt="Banner" 
+        <img
+          className="w-full h-auto object-contain"
+          src={banner}
+          alt="Banner"
         />
       </div>
     </div>
